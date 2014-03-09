328f1ae898320b26925cfd81aef26bfaa304145a
Magdalen Berns <m.berns@thismagpie.com>
2014-03-09 18:33:33 +0000

    Fix various colour issues

--- magpie/category.php
+++ magpie/category.php
@@ -22,6 +22,8 @@ get_header(); ?>
 				?>
 			</header><!-- .archive-header -->
 			<?php
+					get_sidebar( 'content' );
+                    get_sidebar();
 					// Start the Loop.
 					while ( have_posts() ) : the_post();
 
@@ -31,6 +33,7 @@ get_header(); ?>
 					 * (where ___ is the post format) and that will be used instead.
 					 */
 					get_template_part( 'content', get_post_format() );
+					
 
 					endwhile;
 					// Previous/next page navigation.
@@ -42,8 +45,5 @@ get_header(); ?>
 			?>
 		</div><!-- #content -->
 	</section><!-- #primary -->
-</div><!-- #main-content -->
 <?php
-get_sidebar( 'content' );
-get_sidebar();
 get_footer();
